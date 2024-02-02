76660818edc47e5054357c30b8f3772da846cb27
ColumbusDeveloper (Mohmoud) <79301252+ColumbusDevelop@users.noreply.github.com>
2024-02-02 13:48:05 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
+        previous_nums = {}
+
+        for i, n in enumerate(nums):
+            difference = target - n
+
+            if difference in previous_nums:
+                return [i, previous_nums[n]]
+
+            previous_nums[n] = i
